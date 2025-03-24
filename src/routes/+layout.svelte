f83701e81de480a082b78a34a3fc8032e753b01b
wynvernn <jeakarllo@gmail.com>
2025-03-24 15:42:07 -0300

    Add layout and button components, integrate Tailwind CSS, and update configuration files

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+  const { children } = $props();
+  import "../app.css";
+</script>
+
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<p>test</p>
+<script>
+    import Button from "$lib/components/ui/button/button.svelte";
+
+</script>
+<b>OK</b>
+
+<Button>Click me</Button>
